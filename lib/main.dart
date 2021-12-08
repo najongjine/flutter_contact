b752caa55c8b6eba550e9bc0f1440c43cce0abd9
Jong Park <najongjin3@hotmail.com>
2021-12-08 12:51:01 +0900

    .

--- lib/main.dart
+++ lib/main.dart
@@ -14,13 +14,30 @@ class MyApp extends StatelessWidget {
     /* 실제 코딩하는곳 */
     return MaterialApp(
         home: Scaffold(
-          body: Column(
-            children: [
-              Icon(Icons.star),
-              Icon(Icons.star),
-              Icon(Icons.star),
-            ],
-          )
+          appBar: AppBar(title: Text("앱"),),
+          body: Align(
+            alignment: Alignment.centerLeft,
+            child: Container(
+              width: 150,
+              height: 50,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black)
+              ),
+              padding: EdgeInsets.all(20),
+              child: Text("hi"),
+            ),
+          ),
+          bottomNavigationBar: SizedBox(
+            height: 100,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Icon(Icons.phone)
+                ,Icon(Icons.message)
+                ,Icon(Icons.contact_page)
+              ],
+            ),
+          ),
         )
     );
 
